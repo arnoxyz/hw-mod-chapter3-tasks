22241778b875c2ebc821cac59787db394ebe01fb
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-01-19 19:06:58 -0500

    pwm start

--- bcd_fsm/src/bcd_fsm.vhd
+++ bcd_fsm/src/bcd_fsm.vhd
@@ -21,18 +21,47 @@ entity bcd_fsm is
 end entity;
 
 architecture beh of bcd_fsm is
-	-- TODO: Declare an fsm state enum type
+	--Declare an fsm state enum type
+	type fsm_state_t is (IDLE, A, B); 
+
 	-- TODO: Declare a state register record type
+	signal state, state_next: fsm_state_t;
 begin
 
 	sync : process(clk, res_n)
 	begin
 		-- TODO: Implement state register
+
+		--reset all regs
+		if (res_n = '0') then
+			state <= IDLE;
+
+		elsif rising_edge(clk) then
+			state <= state_next;
+		end if;
 	end process;
 
 	comb : process(all)
 	begin
 	-- TODO: Implement combinational logic
+		state_next <= state;
+
+		case state is
+			when IDLE => 
+				leds_internal_next <= (others => '0');
+				state_next <= A;
+
+			when A =>
+				leds_internal_next(LEDS_WIDTH-1) <= '1'; 
+				state_next <= B;
+
+			when B =>
+				leds_internal_next <= std_logic_vector(shift_right((unsigned(leds_internal)),1)); 
+
+				if(leds(0) = '1') then 
+					state_next <= IDLE;
+				end if;
+		end case;
 	end process;
 
 end architecture;
---
+++ pwm/src/pwm_signal_generator.vhd
@@ -0,0 +1,44 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity pwm_signal_generator is
+	generic (
+		COUNTER_WIDTH : integer := 8
+	);
+	port (
+		clk        : in std_ulogic;
+		res_n      : in std_ulogic;
+		en         : in std_ulogic;
+		value      : in std_ulogic_vector(COUNTER_WIDTH-1 downto 0);
+		pwm_out    : out std_ulogic
+	);
+end entity;
+
+
+architecture arch of pwm_signal_generator is
+begin
+
+	sync : process(clk, res_n)
+		variable counter_reg : unsigned(COUNTER_WIDTH-1 downto 0) := (others => '0');
+		variable active : std_ulogic := '0';
+	begin
+		pwm_out <= '0';
+
+		--async reset
+		if (res_n = '0') then
+			counter_reg := (others => '0');
+			active := '0';
+		elsif rising_edge(clk) then
+			if counter_reg = 0 and en = '1' then 
+				active := '1';
+			end if;
+
+			if active = '1' then 
+				if counter_reg >= unsigned(value) then 
+					pwm_out <= '1';
+				end if;
+			end if;
+		end if;
+	end process;
+end architecture;
