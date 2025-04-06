2fd77b8fb82538ff13fadd141fe50a69ea2dee9a
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-04-06 16:55:25 +0200

    simple timer top_arch test on fpga board

---
+++ simple_timer/top_arch.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.math_pkg.all;
+
+architecture arch of top is
+	constant N : positive := 8;
+begin
+
+	testUnit : entity work.timer(beh)
+	generic map (
+    N => N
+	)
+	port map (
+		clk    => clk,
+		res_n  => keys(0),
+		en     => keys(1),
+		tick   => ledg(0),
+	);
+
+end architecture;
