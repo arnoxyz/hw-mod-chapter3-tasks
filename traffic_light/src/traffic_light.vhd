1a977079e7cd7bdc35f162fb2661cf971219509c
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-04-07 17:17:18 +0200

    traffic light, impl. step 4,5 done

--- traffic_light/src/traffic_light.vhd
+++ traffic_light/src/traffic_light.vhd
@@ -29,4 +29,97 @@ architecture beh of traffic_light is
   signal s, s_nxt : fsm_reg_t;
   constant INIT_VAL : fsm_reg_t := (state=>IDLE, sec_cnt=>(others=>'0'), clk_cnt=>(others=>'0'));
 begin 
+
+  sync : process(clk, res_n) is 
+  begin 
+    if res_n = '0' then 
+      s <= INIT_VAL; 
+    elsif rising_edge(clk) then 
+      s <= s_nxt;
+    end if;
+  end process;
+  
+  comb : process(all) is 
+  begin 
+    s_nxt <= s;
+    pl <= "01"; 
+    tl <= "100";
+
+    case s.state is 
+      when IDLE =>
+        if btn_n = '0' then 
+          s_nxt.state <= BLINK_GREEN_ON;
+        end if;
+
+      when BLINK_GREEN_ON=>
+        s_nxt.clk_cnt <= s.clk_cnt + 1;
+
+        if to_integer(s.sec_cnt) >= 8 then 
+          s_nxt.state <= YELLOW1;
+          s_nxt.clk_cnt <= (others => '0');
+          s_nxt.sec_cnt <= (others => '0');
+        end if;
+
+        if s.clk_cnt >= CLK_FREQ then 
+          s_nxt.state <= BLINK_GREEN_OFF;
+          s_nxt.sec_cnt <= s_nxt.sec_cnt + 1;
+          s_nxt.clk_cnt <= (others => '0');
+        end if;
+
+      when BLINK_GREEN_OFF=>
+        s_nxt.clk_cnt <= s.clk_cnt + 1;
+        tl <= "000";
+
+        if s.clk_cnt >= CLK_FREQ then 
+          s_nxt.state <= BLINK_GREEN_ON;
+          s_nxt.sec_cnt <= s_nxt.sec_cnt + 1;
+          s_nxt.clk_cnt <= (others => '0');
+        end if;
+
+      when YELLOW1=>
+      tl <= "010";
+      s_nxt.clk_cnt <= s.clk_cnt + 1;
+
+      if to_integer(s.sec_cnt) >= WAIT_TIME then 
+        s_nxt.state <= RED;
+        s_nxt.clk_cnt <= (others => '0');
+        s_nxt.sec_cnt <= (others => '0');
+      end if;
+
+      if s.clk_cnt >= CLK_FREQ then 
+        s_nxt.sec_cnt <= s_nxt.sec_cnt + 1;
+        s_nxt.clk_cnt <= (others => '0');
+      end if;
+
+      when RED=>
+      pl <= "10"; 
+      tl <= "001";
+
+      if to_integer(s.sec_cnt) >= WAIT_TIME then 
+        s_nxt.state <= YELLOW2;
+        s_nxt.clk_cnt <= (others => '0');
+        s_nxt.sec_cnt <= (others => '0');
+      end if;
+
+      if s.clk_cnt >= CLK_FREQ then 
+        s_nxt.sec_cnt <= s_nxt.sec_cnt + 1;
+        s_nxt.clk_cnt <= (others => '0');
+      end if;
+
+      when YELLOW2=>
+      pl <= "01"; 
+      tl <= "010";
+
+      if to_integer(s.sec_cnt) >= WAIT_TIME then 
+        s_nxt.state <= IDLE;
+        s_nxt.clk_cnt <= (others => '0');
+        s_nxt.sec_cnt <= (others => '0');
+      end if;
+
+      if s.clk_cnt >= CLK_FREQ then 
+        s_nxt.sec_cnt <= s_nxt.sec_cnt + 1;
+        s_nxt.clk_cnt <= (others => '0');
+      end if;
+    end case;
+  end process;
 end architecture;
